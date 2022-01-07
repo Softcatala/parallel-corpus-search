a715a85749e13fdbc9d3cf113daec36b66ff7390
Jordi Mas <jmas@softcatala.org>
2022-01-07 18:10:03 +0100

    Fix process start

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -1,4 +1,3 @@
 #/bin/sh
-cd srv/web/
-mkdir -p /var/log/parallel-corpus-search/
-gunicorn web_corpus:app -b 0.0.0.0:8000 --error-logfile /var/log/parallel-corpus-search/gnuicorn.log --workers=8
+cd /srv/
+gunicorn web_corpus:app -b 0.0.0.0:8000 --workers=8
