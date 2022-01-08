a5654ccac16077c04aa2a937fa5a06b0296cbeeb
Jordi Mas <jmas@softcatala.org>
2022-01-08 19:16:48 +0100

    Easier start and stop

--- elasticsearch_static_data/build.sh
+++ elasticsearch_static_data/build.sh
@@ -20,22 +20,13 @@ mkdir static_data
 sudo sysctl -w vm.max_map_count=262144
 docker pull $IMAGE_NAME
 
-docker run -d -p 9200:9200 -v "$(pwd)/elasticsearch.yml":/usr/share/elasticsearch/config/elasticsearch.yml \
+docker run --rm --name es_build -d -p 9200:9200 -v "$(pwd)/elasticsearch.yml":/usr/share/elasticsearch/config/elasticsearch.yml \
 -v "$(pwd)/static_data":/usr/share/elasticsearch/data -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" $IMAGE_NAME 
 
 echo "Starts inserting"
 python3 insert.py
 
-
-if [[ "$(docker ps -q -f name=$IMAGE_NAME 2> /dev/null)" != "" ]]; then
-    echo Stopping $IMAGE_NAME running container
-    docker stop $IMAGE_NAME
-fi
-
-if [[ "$(docker container ls -a -q -f name=$IMAGE_NAME  2> /dev/null)" != "" ]]; then
-    echo Removing $IMAGE_NAME container
-    docker container rm $IMAGE_NAME
-fi
+docker stop es_build
 
 docker build -t elasticsearch-static . -f dockerfile
 docker image ls | grep elasticsearch-static
